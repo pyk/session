255a075762ca0cfcd8d6f6b928c726886b42d9d4
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-01-09 20:18:42 +0700

    initial commit

--- session.go
+++ session.go
@@ -1,1 +1,201 @@
 package session
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+// define replies
+const (
+	REPLY_220 = "220 Maillennia ready"
+	REPLY_440 = "440 Command not received. Please try again"
+	REPLY_502 = "502 5.5.1 Unrecognized command."
+
+	// transmitted with error
+	REPLY_500 = "500 "
+)
+
+// reply represents a SMTP Replies
+type Reply struct {
+	w *bufio.Writer
+}
+
+// Transmit send a reply to SMTP sender
+func (rp *Reply) Transmit(str string) error {
+	fmt.Fprintf(rp.w, "%s\r\n", str)
+	err := rp.w.Flush()
+	if err != nil {
+		return errors.New("Error while send a Reply")
+	}
+	return nil
+}
+
+// TransmitWithErr send a reply to SMTP sender with custom error message
+func (rp *Reply) TransmitWithErr(str string, err error) error {
+	fmt.Fprintf(rp.w, "%s%s\r\n", str, err.Error())
+	e := rp.w.Flush()
+	if e != nil {
+		return errors.New("Error while send a Reply")
+	}
+	return nil
+}
+
+// command represents a SMTP Commands
+type command string
+
+func (c command) String() string {
+	return string(c)
+}
+
+func (c command) Valid() (bool, error) {
+	// empty lines
+	if c.String() == "" {
+		return false, errors.New("Syntax error")
+	}
+
+	// every command should terminated with <CRLF>
+	if r := strings.HasSuffix(c.String(), "\r\n"); !r {
+		return false, errors.New("Command not terminated with <CRLF>")
+	}
+
+	return true, nil
+}
+func (c command) Verb() string {
+	if c.String() == "\r\n" {
+		return "\r\n"
+	}
+
+	verb := strings.TrimSpace(c.String())
+	if len(verb) == 4 {
+		return strings.ToUpper(verb)
+	}
+	if len(verb) > 4 {
+		i := strings.Index(verb, ":")
+		if i > 0 {
+			return strings.ToUpper(verb[:i+1])
+		} else {
+			s := strings.Split(verb, " ")
+			return strings.ToUpper(s[0])
+		}
+	}
+	return "NOT YET"
+}
+
+func (c command) Arg() string {
+	// TODO: extract argument from command string
+	s := strings.Split(string(c), " ")
+	if len(s) > 1 {
+		return s[1]
+	}
+	return ""
+}
+
+// Session represents session on new connection
+type Session struct {
+	Conn     net.Conn
+	Validity bool
+	Reader   *bufio.Reader
+	Writer   *bufio.Writer
+	Reply    *Reply
+}
+
+// New create a new session
+func New(conn net.Conn) *Session {
+	rp := &Reply{
+		w: bufio.NewWriter(conn),
+	}
+
+	return &Session{
+		Conn:     conn,
+		Validity: false,
+		Reader:   bufio.NewReader(conn),
+		Writer:   bufio.NewWriter(conn),
+		Reply:    rp,
+	}
+}
+
+// Close close the open connection of session
+func (s *Session) Close() error {
+	err := s.Conn.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Session) SetValid(valid bool) {
+	s.Validity = true
+}
+
+func (s *Session) Valid() bool {
+	return s.Validity
+}
+
+// Serve serve connected SMTP sender
+func (s *Session) Serve() {
+	defer s.Close()
+
+	err := s.Reply.Transmit(REPLY_220)
+	if err != nil {
+		return
+	}
+
+	for {
+		// read from connection, return non-escaped string include \r\n
+		line, err := s.Reader.ReadString('\n')
+		if err != nil {
+			err := s.Reply.Transmit(REPLY_440)
+			if err != nil {
+				return
+			}
+		}
+
+		c := command(line)
+		valid, err := c.Valid()
+		if !valid && err != nil {
+			// send a reply with custom error
+			e := s.Reply.TransmitWithErr(REPLY_500, err)
+			if e != nil {
+				return
+			}
+		}
+
+		switch c.Verb() {
+		case "\r\n":
+			log.Println("enter")
+		case "EHLO":
+			log.Println(c.Verb())
+		case "HELO":
+			log.Println(c.Verb())
+		case "DATA":
+			log.Println(c.Verb())
+		case "RSET":
+			log.Println(c.Verb())
+		case "QUIT":
+			log.Println(c.Verb())
+		case "NOOP":
+			log.Println(c.Verb())
+		case "HELP":
+			log.Println(c.Verb())
+		case "EXPN":
+			log.Println(c.Verb())
+		case "VRFY":
+			log.Println(c.Verb())
+		case "MAIL FROM:":
+			log.Println(c.Verb())
+		case "RCPT TO:":
+			log.Println(c.Verb())
+		default:
+			err := errors.New("Command unrecognized")
+			e := s.Reply.TransmitWithErr(REPLY_500, err)
+			if e != nil {
+				return
+			}
+		}
+
+	}
+}
--- session_test.go
+++ session_test.go
@@ -1,1 +1,75 @@
 package session
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestCheckValidalityOfCommand(t *testing.T) {
+	cases := []struct {
+		line  string
+		valid bool
+		err   error
+	}{
+		// command should terminated with <CRLF>
+		{"", false, errors.New("Syntax error")},
+		{"\r\n", true, nil},
+		{"HELLO", false, errors.New("Command not terminated with <CRLF>")},
+		{"EHLO ubuntu-trusty\r\n", true, nil},
+	}
+
+	for _, input := range cases {
+		c := command(input.line)
+		got, err := c.Valid()
+		// check validality
+		if got != input.valid {
+			t.Errorf("%q.Valid() == %t, expected %t", input.line, got, input.valid)
+		}
+		// check error message
+		if err != input.err {
+			if err.Error() != input.err.Error() {
+				t.Errorf("got %v, expected %v", err, input.err)
+			}
+		}
+	}
+}
+
+func TestCommandVerb(t *testing.T) {
+	cases := []struct {
+		valid_line, expected_verb string
+	}{
+		{"\r\n", "\r\n"},
+		{"DATA\r\n", "DATA"},
+		{"data\r\n", "DATA"},
+		{"RSET\r\n", "RSET"},
+		{"rset\r\n", "RSET"},
+		{"QUIT\r\n", "QUIT"},
+		{"quit\r\n", "QUIT"},
+		{"NOOP\r\n", "NOOP"},
+		{"noop\r\n", "NOOP"},
+		{"EHLO some-string\r\n", "EHLO"},
+		{"HELO some-string\r\n", "HELO"},
+		{"ehlo some-string\r\n", "EHLO"},
+		{"helo some-string\r\n", "HELO"},
+		{"NOOP some-string\r\n", "NOOP"},
+		{"noop some-string\r\n", "NOOP"},
+		{"HELP some-string\r\n", "HELP"},
+		{"HELP some-string\r\n", "HELP"},
+		{"EXPN some-string\r\n", "EXPN"},
+		{"expn some-string\r\n", "EXPN"},
+		{"VRFY some-string\r\n", "VRFY"},
+		{"vrfy some-string\r\n", "VRFY"},
+
+		{"MAIL FROM: some-string\r\n", "MAIL FROM:"},
+		{"mail from: some-string\r\n", "MAIL FROM:"},
+		{"RCPT TO: some-string\r\n", "RCPT TO:"},
+		{"rcpt to: some-string\r\n", "RCPT TO:"},
+	}
+
+	for _, input := range cases {
+		verb := command(input.valid_line).Verb()
+		if verb != input.expected_verb {
+			t.Errorf("%q: got %q, expected %q", input.valid_line, verb, input.expected_verb)
+		}
+	}
+}
